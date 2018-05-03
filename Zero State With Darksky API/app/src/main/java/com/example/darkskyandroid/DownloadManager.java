96a81e9af4e3d51f406ae04027e4561dddbc4e29
Kevin Miskovich <kmiskovich@gmail.com>
2018-05-03 16:55:22 -0400

    Zero State and Darksky API Push

---
+++ Zero State With Darksky API/app/src/main/java/com/example/darkskyandroid/DownloadManager.java
@@ -0,0 +1,10 @@
+package com.example.darkskyandroid;
+
+/**
+ * Created by Nick on 10/20/2015.
+ */
+public interface DownloadManager {
+    void onDownloadSuccess(String response);
+    void onDownloadFailed(String response);
+    void onNotDownloaded();
+}
